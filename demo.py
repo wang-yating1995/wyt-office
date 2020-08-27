9adbc5f605df1f8ec054575b349a684eb59d723c
test01 <1103120302@qq.com>
2020-08-27 21:27:56 +0800

    wyt-office

--- demo.py
+++ demo.py
@@ -1,4 +1,20 @@
-print("xixi")
+print("xixi",end = "")
 print(2)
 print(2.33)
 print(True,False)
+print(())
+print([])
+print({})
+print(None)
+print("haha"+"1")
+print("haha","xixi",123,False)
+
+a = input("请输入第一个数字：")
+b = input("请输入第二个数字：")
+print(int(a)+int(b))
+print(type(int(a)+int(b)))
+
+c = int("1")
+print(type(c))
+
+print(len("哈哈"))
